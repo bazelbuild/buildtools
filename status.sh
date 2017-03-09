8c985596492cee3b7d0b8d53c30e377fb67d9685
Rishi <rishiloyola98245@gmail.com>
2017-03-10 00:31:35 +0530

    Remove if conditions by set command

--- status.sh
+++ status.sh
@@ -1,18 +1,10 @@
 #!/bin/bash
 
+set -e
+
 buildifier_tags=$(git describe --tags)
 IFS='-' read -a parse_tags <<< "$buildifier_tags"
-if [[ $? != 0 ]];
-then
-    exit 1
-fi
 echo "BUILDIFIER_VERSION ${parse_tags[0]}"
 
-
 buildifier_rev=$(git rev-parse HEAD)
-if [[ $? != 0 ]];
-then
-    exit 1
-fi
-
 echo "BUILD_SCM_REVISION ${buildifier_rev}"
