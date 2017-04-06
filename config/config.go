bc44e88f188260b3aeac475054addc24285b4645
Paul Bethe <pbethe@google.com>
2017-04-06 11:38:26 -0400

    Import from Google: unused_deps script for trimming deps from java_library

---
+++ config/config.go
@@ -0,0 +1,25 @@
+/*
+Copyright 2017 Google Inc. All Rights Reserved.
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+    http://www.apache.org/licenses/LICENSE-2.0
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+ WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ See the License for the specific language governing permissions and
+ limitations under the License.
+*/
+
+// Package config provides environment specific configuration elements to unused_deps
+package config
+
+var (
+	DefaultBuildTool        = "bazel"
+	BuildToolHelp           = "the build executable (like bazel)"
+	ExtraActionFileNameHelp = "When specified, just prints suspected unused deps."
+	// DefaultBinDir to look for outputs
+	DefaultBinDir = "bazel-bin"
+	// DefaultExtraBuildFlags is internal-only
+	DefaultExtraBuildFlags = []string{}
+)
