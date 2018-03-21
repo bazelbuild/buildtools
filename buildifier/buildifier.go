f987991eea10a1470472ab40a8fe9cca7b9cc5fc
Vladimir Moskva <vladmos@users.noreply.github.com>
2018-03-21 13:35:55 +0100

    Change a flag for switching between build and bzl modes (#232)

--- buildifier/buildifier.go
+++ buildifier/buildifier.go
@@ -52,7 +52,7 @@ var (
 	disable   = stringList("buildifier_disable", "list of buildifier rewrites to disable")
 
 	// Experimental flags
-	formatBzlFiles = flag.Bool("format_bzl", false, "format bzl-specific blocks (experimental)")
+	inputType = flag.String("type", "build", "Experimental: input file type: build (default, for BUILD files) or bzl (for .bzl files).")
 )
 
 func stringList(name, help string) func() []string {
@@ -105,7 +105,18 @@ func main() {
 	build.DisableRewrites = disable()
 	build.AllowSort = allowSort()
 
-	tables.FormatBzlFiles = *formatBzlFiles
+	// Check input type.
+	switch *inputType {
+	case "bzl":
+		tables.FormatBzlFiles = true
+
+	case "build", "":
+		tables.FormatBzlFiles = false
+
+	default:
+		fmt.Fprintf(os.Stderr, "buildifier: unrecognized input type %s; valid types are build, bzl\n", *inputType)
+		os.Exit(2)
+	}
 
 	if *dflag {
 		if *mode != "" {
@@ -117,15 +128,15 @@ func main() {
 
 	// Check mode.
 	switch *mode {
-	default:
-		fmt.Fprintf(os.Stderr, "buildifier: unrecognized mode %s; valid modes are check, diff, fix\n", *mode)
-		os.Exit(2)
-
 	case "":
 		*mode = "fix"
 
 	case "check", "diff", "fix", "print_if_changed":
 		// ok
+
+	default:
+		fmt.Fprintf(os.Stderr, "buildifier: unrecognized mode %s; valid modes are check, diff, fix\n", *mode)
+		os.Exit(2)
 	}
 
 	// If the path flag is set, must only be formatting a single file.
