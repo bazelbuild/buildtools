4306203da5b965a51bc6e46de4a50c0286eb2971
Laurent Le Brun <laurentlb@gmail.com>
2018-06-13 11:22:51 +0200

    Allow postfix traversal of the AST. (#328)
    Add WalkOnce, a basic function to implement AST traversals.
    
    It allows both prefix and postfix operations, as well as partial AST walks.

--- build/walk.go
+++ build/walk.go
@@ -30,7 +30,7 @@ func Walk(v Expr, f func(x Expr, stk []Expr)) {
 	})
 }
 
-// WalkAndUpdate walks the expression tree v, calling f on all subexpressions
+// Edit walks the expression tree v, calling f on all subexpressions
 // in a preorder traversal. If f returns a non-nil value, the tree is mutated.
 // The new value replaces the old one.
 //
@@ -42,12 +42,7 @@ func Edit(v Expr, f func(x Expr, stk []Expr) Expr) Expr {
 	return walk1(&v, &stack, f)
 }
 
-// walk1 is the actual implementation of Walk and WalkAndUpdate.
-// It has the same signature and meaning as Walk,
-// except that it maintains in *stack the current stack
-// of nodes. Using a pointer to a slice here ensures that
-// as the stack grows and shrinks the storage can be
-// reused for the next growth.
+// walk1 is a helper function for Walk, WalkWithPostfix, and Edit.
 func walk1(v *Expr, stack *[]Expr, f func(x Expr, stk []Expr) Expr) Expr {
 	if v == nil {
 		return nil
@@ -57,106 +52,114 @@ func walk1(v *Expr, stack *[]Expr, f func(x Expr, stk []Expr) Expr) Expr {
 		*v = res
 	}
 	*stack = append(*stack, *v)
-	switch v := (*v).(type) {
-	case *File:
-		for _, stmt := range v.Stmt {
-			walk1(&stmt, stack, f)
-		}
-	case *DotExpr:
-		walk1(&v.X, stack, f)
-	case *IndexExpr:
-		walk1(&v.X, stack, f)
-		walk1(&v.Y, stack, f)
-	case *KeyValueExpr:
-		walk1(&v.Key, stack, f)
-		walk1(&v.Value, stack, f)
-	case *SliceExpr:
-		walk1(&v.X, stack, f)
-		if v.From != nil {
-			walk1(&v.From, stack, f)
-		}
-		if v.To != nil {
-			walk1(&v.To, stack, f)
-		}
-		if v.Step != nil {
-			walk1(&v.Step, stack, f)
-		}
-	case *ParenExpr:
-		walk1(&v.X, stack, f)
-	case *UnaryExpr:
-		walk1(&v.X, stack, f)
-	case *BinaryExpr:
-		walk1(&v.X, stack, f)
-		walk1(&v.Y, stack, f)
-	case *LambdaExpr:
-		for i := range v.Params {
-			walk1(&v.Params[i], stack, f)
-		}
-		for i := range v.Body {
-			walk1(&v.Body[i], stack, f)
-		}
-	case *CallExpr:
-		walk1(&v.X, stack, f)
-		for i := range v.List {
-			walk1(&v.List[i], stack, f)
-		}
-	case *ListExpr:
-		for i := range v.List {
-			walk1(&v.List[i], stack, f)
-		}
-	case *SetExpr:
-		for i := range v.List {
-			walk1(&v.List[i], stack, f)
-		}
-	case *TupleExpr:
-		for i := range v.List {
-			walk1(&v.List[i], stack, f)
-		}
-	case *DictExpr:
-		for i := range v.List {
-			walk1(&v.List[i], stack, f)
-		}
-	case *Comprehension:
-		walk1(&v.Body, stack, f)
-		for _, c := range v.Clauses {
-			walk1(&c, stack, f)
-		}
-	case *IfClause:
-		walk1(&v.Cond, stack, f)
-	case *ForClause:
-		walk1(&v.Vars, stack, f)
-		walk1(&v.X, stack, f)
-	case *ConditionalExpr:
-		walk1(&v.Then, stack, f)
-		walk1(&v.Test, stack, f)
-		walk1(&v.Else, stack, f)
-	case *DefStmt:
-		for _, p := range v.Params {
-			walk1(&p, stack, f)
-		}
-		for _, s := range v.Body {
-			walk1(&s, stack, f)
-		}
-	case *IfStmt:
-		walk1(&v.Cond, stack, f)
-		for _, s := range v.True {
-			walk1(&s, stack, f)
-		}
-		for _, s := range v.False {
-			walk1(&s, stack, f)
-		}
-	case *ForStmt:
-		walk1(&v.Vars, stack, f)
-		walk1(&v.X, stack, f)
-		for _, s := range v.Body {
-			walk1(&s, stack, f)
-		}
-	case *ReturnStmt:
-		if v.Result != nil {
-			walk1(&v.Result, stack, f)
-		}
-	}
+
+	WalkOnce(*v, func(x *Expr) {
+		walk1(x, stack, f)
+	})
 
 	*stack = (*stack)[:len(*stack)-1]
 	return *v
+}
+
+// WalkOnce calls f on every child of v.
+func WalkOnce(v Expr, f func(x *Expr)) {
+	switch v := v.(type) {
+	case *File:
+		for _, stmt := range v.Stmt {
+			f(&stmt)
+		}
+	case *DotExpr:
+		f(&v.X)
+	case *IndexExpr:
+		f(&v.X)
+		f(&v.Y)
+	case *KeyValueExpr:
+		f(&v.Key)
+		f(&v.Value)
+	case *SliceExpr:
+		f(&v.X)
+		if v.From != nil {
+			f(&v.From)
+		}
+		if v.To != nil {
+			f(&v.To)
+		}
+		if v.Step != nil {
+			f(&v.Step)
+		}
+	case *ParenExpr:
+		f(&v.X)
+	case *UnaryExpr:
+		f(&v.X)
+	case *BinaryExpr:
+		f(&v.X)
+		f(&v.Y)
+	case *LambdaExpr:
+		for i := range v.Params {
+			f(&v.Params[i])
+		}
+		for i := range v.Body {
+			f(&v.Body[i])
+		}
+	case *CallExpr:
+		f(&v.X)
+		for i := range v.List {
+			f(&v.List[i])
+		}
+	case *ListExpr:
+		for i := range v.List {
+			f(&v.List[i])
+		}
+	case *SetExpr:
+		for i := range v.List {
+			f(&v.List[i])
+		}
+	case *TupleExpr:
+		for i := range v.List {
+			f(&v.List[i])
+		}
+	case *DictExpr:
+		for i := range v.List {
+			f(&v.List[i])
+		}
+	case *Comprehension:
+		f(&v.Body)
+		for _, c := range v.Clauses {
+			f(&c)
+		}
+	case *IfClause:
+		f(&v.Cond)
+	case *ForClause:
+		f(&v.Vars)
+		f(&v.X)
+	case *ConditionalExpr:
+		f(&v.Then)
+		f(&v.Test)
+		f(&v.Else)
+	case *DefStmt:
+		for _, p := range v.Params {
+			f(&p)
+		}
+		for _, s := range v.Body {
+			f(&s)
+		}
+	case *IfStmt:
+		f(&v.Cond)
+		for _, s := range v.True {
+			f(&s)
+		}
+		for _, s := range v.False {
+			f(&s)
+		}
+	case *ForStmt:
+		f(&v.Vars)
+		f(&v.X)
+		for _, s := range v.Body {
+			f(&s)
+		}
+	case *ReturnStmt:
+		if v.Result != nil {
+			f(&v.Result)
+		}
+	}
 }
---
+++ build/walk_test.go
@@ -0,0 +1,52 @@
+package build
+
+import "testing"
+
+func nodeToString(e Expr) string {
+	if bin, ok := e.(*BinaryExpr); ok {
+		return bin.Op
+	}
+	if lit, ok := e.(*LiteralExpr); ok {
+		return lit.Token
+	}
+	return "unknown"
+}
+
+// (1 + 2) * (3 - 4)
+var binaryExprExample Expr = &BinaryExpr{
+	X: &BinaryExpr{
+		X:  &LiteralExpr{Token: "1"},
+		Op: "+",
+		Y:  &LiteralExpr{Token: "2"},
+	},
+	Op: "*",
+	Y: &BinaryExpr{
+		X:  &LiteralExpr{Token: "3"},
+		Op: "-",
+		Y:  &LiteralExpr{Token: "4"},
+	},
+}
+
+func TestWalk(t *testing.T) {
+	var prefix []string
+	Walk(binaryExprExample, func(e Expr, stk []Expr) {
+		prefix = append(prefix, nodeToString(e))
+	})
+	compare(t, prefix, []string{"*", "+", "1", "2", "-", "3", "4"})
+}
+
+func TestWalkOnce(t *testing.T) {
+	var prefix []string
+	var postfix []string
+
+	var walk func(e *Expr)
+	walk = func(e *Expr) {
+		prefix = append(prefix, nodeToString(*e))
+		WalkOnce(*e, walk)
+		postfix = append(postfix, nodeToString(*e))
+	}
+	walk(&binaryExprExample)
+
+	compare(t, prefix, []string{"*", "+", "1", "2", "-", "3", "4"})
+	compare(t, postfix, []string{"1", "2", "+", "3", "4", "-", "*"})
+}
