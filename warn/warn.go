80118e79ea3469cbe153bb557e4066c92ac5e279
Keith Smiley <keithbsmiley@gmail.com>
2018-11-28 11:52:54 -0800

    Exit failure if buildifier lint has warnings (#460)

--- warn/warn.go
+++ warn/warn.go
@@ -975,7 +975,7 @@ func FileWarnings(f *build.File, pkg string, enabledWarnings []string, fix bool)
 // PrintWarnings prints the list of warnings returned from calling FileWarnings.
 // Actionable warnings list their link in parens, inactionable warnings list
 // their link in square brackets.
-func PrintWarnings(f *build.File, pkg string, enabledWarnings []string, showReplacements bool) {
+func PrintWarnings(f *build.File, pkg string, enabledWarnings []string, showReplacements bool) bool {
 	warnings := FileWarnings(f, pkg, enabledWarnings, false)
 	sort.Slice(warnings, func(i, j int) bool { return warnings[i].Start.Line < warnings[j].Start.Line })
 	for _, w := range warnings {
@@ -999,6 +999,8 @@ func PrintWarnings(f *build.File, pkg string, enabledWarnings []string, showRepl
 			fmt.Fprintf(os.Stderr, "\n")
 		}
 	}
+
+	return len(warnings) > 0
 }
 
 // FixWarnings fixes all warnings that can be fixed automatically.
