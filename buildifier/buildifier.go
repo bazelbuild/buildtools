d836798fb4c8aba1dacce1c5c41a0d9c70995f9b
Vladimir Moskva <vladmos@google.com>
2019-04-04 14:01:02 +0200

    Allow --lint=warn to be combined with any value of --mode

--- buildifier/buildifier.go
+++ buildifier/buildifier.go
@@ -129,92 +129,15 @@ func main() {
 	build.DisableRewrites = disable()
 	build.AllowSort = allowSort()
 
-	// Check input type.
-	switch *inputType {
-	case "build", "bzl", "workspace", "default", "auto":
-		// ok
-
-	default:
-		fmt.Fprintf(os.Stderr, "buildifier: unrecognized input type %s; valid types are build, bzl, workspace, default, auto\n", *inputType)
+	if err := utils.ValidateModes(inputType, mode, lint, dflag); err != nil {
+		fmt.Fprintf(os.Stderr, err.Error())
 		os.Exit(2)
 	}
 
-	if *dflag {
-		if *mode != "" {
-			fmt.Fprintf(os.Stderr, "buildifier: cannot specify both -d and -mode flags\n")
-			os.Exit(2)
-		}
-		*mode = "diff"
-	}
-
-	// Check mode.
-	switch *mode {
-	case "":
-		*mode = "fix"
-
-	case "check", "diff", "fix", "print_if_changed":
-		// ok
-
-	default:
-		fmt.Fprintf(os.Stderr, "buildifier: unrecognized mode %s; valid modes are check, diff, fix\n", *mode)
+	warningsList, err := utils.ValidateWarnings(warnings)
+	if err != nil {
+		fmt.Fprintf(os.Stderr, err.Error())
 		os.Exit(2)
-	}
-
-	// Check lint mode.
-	switch *lint {
-	case "":
-		*lint = "off"
-
-	case "off":
-		// ok
-
-	case "warn", "fix":
-		if *mode != "fix" {
-			fmt.Fprintf(os.Stderr, "buildifier: lint mode %s is only compatible with --mode=fix\n", *lint)
-			os.Exit(2)
-		}
-
-	default:
-		fmt.Fprintf(os.Stderr, "buildifier: unrecognized lint mode %s; valid modes are warn and fix\n", *lint)
-		os.Exit(2)
-	}
-
-	// Check lint warnings
-	var warningsList []string
-	switch *warnings {
-	case "", "default":
-		warningsList = warn.DefaultWarnings
-	case "all":
-		warningsList = warn.AllWarnings
-	default:
-		// Either all or no warning categories should start with "+" or "-".
-		// If all of them start with "+" or "-", the semantics is
-		// "default set of warnings + something - something".
-		plus := map[string]bool{}
-		minus := map[string]bool{}
-		for _, warning := range strings.Split(*warnings, ",") {
-			if strings.HasPrefix(warning, "+") {
-				plus[warning[1:]] = true
-			} else if strings.HasPrefix(warning, "-") {
-				minus[warning[1:]] = true
-			} else {
-				warningsList = append(warningsList, warning)
-			}
-		}
-		if len(warningsList) > 0 && (len(plus) > 0 || len(minus) > 0) {
-			fmt.Fprintf(os.Stderr, "buildifier: warning categories with modifiers (\"+\" or \"-\") can't me mixed with raw warning categories\n")
-			os.Exit(2)
-		}
-		if len(warningsList) == 0 {
-			for _, warning := range warn.DefaultWarnings {
-				if !minus[warning] {
-					warningsList = append(warningsList, warning)
-				}
-			}
-			for warning := range plus {
-				warningsList = append(warningsList, warning)
-			}
-		}
 	}
 
 	// If the path flag is set, must only be formatting a single file.
