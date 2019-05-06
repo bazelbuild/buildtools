1835ea031dbae0baa41bfdd89820c1aeff1e30bf
Vladimir Moskva <vladmos@google.com>
2019-05-06 15:31:52 +0200

    Fix golint warnigns (#625)

--- buildifier/utils/tempfile.go
+++ buildifier/utils/tempfile.go
@@ -6,6 +6,7 @@ import (
 	"os"
 )
 
+// TempFile keeps track of temporary files and cleans them up in the end
 type TempFile struct {
 	filenames []string
 }
@@ -16,7 +17,13 @@ func (tf *TempFile) WriteTemp(data []byte) (file string, err error) {
 	if err != nil {
 		return "", fmt.Errorf("creating temporary file: %v", err)
 	}
-	defer f.Close()
+	defer func() {
+		e := f.Close()
+		if e != nil {
+			err = e
+		}
+	}()
+
 	name := f.Name()
 	if _, err := f.Write(data); err != nil {
 		return "", fmt.Errorf("writing temporary file: %v", err)
@@ -25,8 +32,13 @@ func (tf *TempFile) WriteTemp(data []byte) (file string, err error) {
 	return name, nil
 }
 
-func (tf *TempFile) Clean() {
+// Clean removes all created temporary files
+func (tf *TempFile) Clean() error {
 	for _, file := range tf.filenames {
-		os.Remove(file)
+		if err := os.Remove(file); err != nil {
+			return err
+		}
 	}
+	tf.filenames = []string{}
+	return nil
 }
