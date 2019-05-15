e39fbfad883c74d5ee4638c734f406b2be555bc3
Vladimir Moskva <vladmos@google.com>
2019-05-15 15:57:43 +0200

    Rewrite status.sh in Python (#631)

---
+++ status.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python
+
+from __future__ import print_function, unicode_literals
+from subprocess import Popen, PIPE
+from sys import exit
+
+
+def run(*cmd):
+    process = Popen(cmd, stdout=PIPE)
+    output, err = process.communicate()
+    if process.wait():
+        exit(1)
+    return output.strip()
+
+
+def main():
+    tags = run("git", "describe", "--tags")
+    print("STABLE_buildVersion", tags.split(b"-")[0])
+
+    revision = run("git", "rev-parse", "HEAD")
+    print("STABLE_buildScmRevision", revision)
+
+
+if __name__ == "__main__":
+    main()
