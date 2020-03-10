f2e97c2efc9b6a51ec06cf78bc505f8279765c6d
Vladimir Moskva <vladmos@google.com>
2020-03-10 15:42:06 +0100

    Mute false positive warnings for private .bzl files

--- warn/warn_visibility_test.go
+++ warn/warn_visibility_test.go
@@ -34,12 +34,42 @@ baz()
 		scopeEverywhere)
 
 	checkFindings(t, "bzl-visibility", `
+load("@repo//foo/bar/internal:module.bzl", "foo")
+load("@repo//foo/bar/private:module.bzl", "bar")
+load("@repo//foo/bar/external:module.bzl", "baz")
+load("@repo/internal:module.bzl", "qux")  # incorrect, but shouldn't cause buildifier crashes
+
+
+foo()
+bar()
+baz()
+qux()
+`,
+		[]string{
+			`:1: Module "@repo//foo/bar/internal:module.bzl" can only be loaded from files located inside "//foo/bar", not from "//test/package".`,
+			`:2: Module "@repo//foo/bar/private:module.bzl" can only be loaded from files located inside "//foo/bar", not from "//test/package".`,
+			`:4: Module "@repo/internal:module.bzl" can only be loaded from files located inside "@repo", not from "//test/package".`,
+		},
+		scopeEverywhere)
+
+	checkFindings(t, "bzl-visibility", `
 load("//test/internal/foo:module.bzl", "foo")
 load("//test/private/foo:module.bzl", "bar")
 load("//test/external/foo:module.bzl", "baz")
 load("//test/internal:module.bzl", "foo")
 load("//test/private:module.bzl", "bar")
 load("//test/external:module.bzl", "baz")
+`,
+		[]string{},
+		scopeEverywhere)
+
+	checkFindings(t, "bzl-visibility", `
+load("@repo//test/internal/foo:module.bzl", "foo")
+load("@repo//test/private/foo:module.bzl", "bar")
+load("@repo//test/external/foo:module.bzl", "baz")
+load("@repo//test/internal:module.bzl", "foo")
+load("@repo//test/private:module.bzl", "bar")
+load("@repo//test/external:module.bzl", "baz")
 `,
 		[]string{},
 		scopeEverywhere)
