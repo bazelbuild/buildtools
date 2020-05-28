bazelbuild/buildtools

commit 19db42aa7a206a52b67bc66477c069ca83d092f4

Fix recursive traversal of BUILD files for "..." targets (#732)

edit/buildozer.go
@@ -25,7 +25,6 @@ import (
 	"log"
 	"os"
 	"os/exec"
-	"path"
 	"path/filepath"
 	"regexp"
 	"runtime"
@@ -35,6 +34,7 @@ import (
 	apipb "github.com/bazelbuild/buildtools/api_proto"
 	"github.com/bazelbuild/buildtools/build"
 	"github.com/bazelbuild/buildtools/file"
+	"github.com/bazelbuild/buildtools/wspace"
 	"github.com/golang/protobuf/proto"
 )
 
@@ -947,22 +947,32 @@ func runBuildifier(opts *Options, f *build.File) ([]byte, error) {
 
 // Given a target, whose package may contain a trailing "/...", returns all
 // extisting BUILD file paths which match the package.
-func targetExpressionToBuildFiles(opts *Options, target string) []string {
-	file, _, _ := InterpretLabelForWorkspaceLocation(opts.RootDir, target)
-	if opts.RootDir == "" {
-		var err error
-		if file, err = filepath.Abs(file); err != nil {
-			fmt.Printf("Cannot make path absolute: %s\n", err.Error())
-			os.Exit(1)
+func targetExpressionToBuildFiles(rootDir string, target string) []string {
+	if strings.Contains(target, "/...") {
+		absoluteRoot, _ := wspace.FindWorkspaceRoot(rootDir)
+		_, pkg, _ := ParseLabel(target)
+		// if we have /... somewhere in the target ParseLabel will leave that at the end
+		pkg = strings.TrimSuffix(pkg, "...")
+		return findBuildFiles(filepath.Join(absoluteRoot, pkg))
+	} else {
+		file, _, _ := InterpretLabelForWorkspaceLocation(rootDir, target)
+		if rootDir == "" {
+			var err error
+			if file, err = filepath.Abs(file); err != nil {
+				fmt.Printf("Cannot make path absolute: %s\n", err.Error())
+				os.Exit(1)
+			}
 		}
-	}
-
-	if !strings.HasSuffix(file, "/.../BUILD") {
 		return []string{file}
 	}
 
+}
+
+// Given a root directory, returns all "BUILD" files in that subtree recursively.
+func findBuildFiles(rootDir string) []string {
 	var buildFiles []string
-	searchDirs := []string{strings.TrimSuffix(file, "/.../BUILD")}
+	searchDirs := []string{rootDir}
+
 	for len(searchDirs) != 0 {
 		lastIndex := len(searchDirs) - 1
 		dir := searchDirs[lastIndex]
@@ -975,9 +985,9 @@ func targetExpressionToBuildFiles(opts *Options, target string) []string {
 
 		for _, dirFile := range dirFiles {
 			if dirFile.IsDir() {
-				searchDirs = append(searchDirs, path.Join(dir, dirFile.Name()))
+				searchDirs = append(searchDirs, filepath.Join(dir, dirFile.Name()))
 			} else if _, ok := buildFileNamesSet[dirFile.Name()]; ok {
-				buildFiles = append(buildFiles, path.Join(dir, dirFile.Name()))
+				buildFiles = append(buildFiles, filepath.Join(dir, dirFile.Name()))
 			}
 		}
 	}
@@ -990,15 +1000,17 @@ func targetExpressionToBuildFiles(opts *Options, target string) []string {
 func appendCommands(opts *Options, commandMap map[string][]commandsForTarget, args []string) {
 	commands, targets := parseCommands(args)
 	for _, target := range targets {
-		if strings.HasSuffix(target, "/BUILD") {
-			target = strings.TrimSuffix(target, "/BUILD") + ":__pkg__"
+		for _, buildFileName := range buildFileNames {
+			if strings.HasSuffix(target, filepath.FromSlash("/"+buildFileName)) {
+				target = strings.TrimSuffix(target, filepath.FromSlash("/"+buildFileName)) + ":__pkg__"
+			}
 		}
 		var buildFiles []string
 		_, pkg, _ := ParseLabel(target)
 		if pkg == stdinPackageName {
 			buildFiles = []string{stdinPackageName}
 		} else {
-			buildFiles = targetExpressionToBuildFiles(opts, target)
+			buildFiles = targetExpressionToBuildFiles(opts.RootDir, target)
 		}
 
 		for _, file := range buildFiles {

edit/buildozer_test.go
@@ -14,6 +14,10 @@ distributed under the License is distributed on an "AS IS" BASIS,
 package edit
 
 import (
+	"io/ioutil"
+	"os"
+	"path/filepath"
+	"reflect"
 	"strings"
 	"testing"
 
@@ -113,4 +117,70 @@ func TestCmdRemoveComment(t *testing.T) {
 			t.Errorf("cmdRemoveComment(%d):\ngot:\n%s\nexpected:\n%s", i, got, tt.expected)
 		}
 	}
+}
+
+type targetExpressionToBuildFilesTestCase struct {
+	rootDir, target string
+	buildFiles      []string
+}
+
+func runTestTargetExpressionToBuildFiles(t *testing.T, buildFileName string) {
+	tmp, err := ioutil.TempDir("", "")
+	if err != nil {
+		t.Fatal(err)
+	}
+	defer os.RemoveAll(tmp)
+	if err := os.MkdirAll(filepath.Join(tmp, "a", "b"), 0755); err != nil {
+		t.Fatal(err)
+	}
+	if err := os.MkdirAll(filepath.Join(tmp, "a", "c"), 0755); err != nil {
+		t.Fatal(err)
+	}
+	if err := ioutil.WriteFile(filepath.Join(tmp, "WORKSPACE"), nil, 0755); err != nil {
+		t.Fatal(err)
+	}
+	if err := ioutil.WriteFile(filepath.Join(tmp, buildFileName), nil, 0755); err != nil {
+		t.Fatal(err)
+	}
+	if err := ioutil.WriteFile(filepath.Join(tmp, "a", buildFileName), nil, 0755); err != nil {
+		t.Fatal(err)
+	}
+	if err := ioutil.WriteFile(filepath.Join(tmp, "a", "b", buildFileName), nil, 0755); err != nil {
+		t.Fatal(err)
+	}
+	if err := ioutil.WriteFile(filepath.Join(tmp, "a", "c", buildFileName), nil, 0755); err != nil {
+		t.Fatal(err)
+	}
+
+	for _, tc := range []targetExpressionToBuildFilesTestCase{
+		{tmp, "//", []string{filepath.Join(tmp, buildFileName)}},
+		{tmp, "//:foo", []string{filepath.Join(tmp, buildFileName)}},
+		{tmp, "//a", []string{filepath.Join(tmp, "a", buildFileName)}},
+		{tmp, "//a:foo", []string{filepath.Join(tmp, "a", buildFileName)}},
+		{tmp, "//a/b", []string{filepath.Join(tmp, "a", "b", buildFileName)}},
+		{tmp, "//a/b:foo", []string{filepath.Join(tmp, "a", "b", buildFileName)}},
+		{tmp, "//...", []string{filepath.Join(tmp, buildFileName), filepath.Join(tmp, "a", buildFileName), filepath.Join(tmp, "a", "b", buildFileName), filepath.Join(tmp, "a", "c", buildFileName)}},
+		{tmp, "//a/...", []string{filepath.Join(tmp, "a", buildFileName), filepath.Join(tmp, "a", "b", buildFileName), filepath.Join(tmp, "a", "c", buildFileName)}},
+		{tmp, "//a/b/...", []string{filepath.Join(tmp, "a", "b", buildFileName)}},
+		{tmp, "//a/c/...", []string{filepath.Join(tmp, "a", "c", buildFileName)}},
+		{tmp, "//a/c/...:foo", []string{filepath.Join(tmp, "a", "c", buildFileName)}},
+	} {
+		buildFiles := targetExpressionToBuildFiles(tc.rootDir, tc.target)
+		expectedBuildFilesMap := make(map[string]bool)
+		buildFilesMap := make(map[string]bool)
+		for _, buildFile := range buildFiles {
+			buildFilesMap[buildFile] = true
+		}
+		for _, buildFile := range tc.buildFiles {
+			expectedBuildFilesMap[buildFile] = true
+		}
+		if !reflect.DeepEqual(expectedBuildFilesMap, buildFilesMap) {
+			t.Errorf("TargetExpressionToBuildFiles(%q, %q) = %q want %q", tc.rootDir, tc.target, buildFiles, tc.buildFiles)
+		}
+	}
+}
+
+func TestTargetExpressionToBuildFiles(t *testing.T) {
+	runTestTargetExpressionToBuildFiles(t, "BUILD")
+	runTestTargetExpressionToBuildFiles(t, "BUILD.bazel")
 }

commit b210ee76de96a50ccbcfe34c568bd74913f1321f

Fix windows handling of path to build file containing "/"
Copybara invokes buildozer by passing the relative path to a
BUILD file using Unix separator.
This was broken in 19db42aa7a by the introduction
of `filepath.FromSlash` when evaluating the path to BUILD files.
Instead we can make it more tolerant by accepting both forms.

edit/buildozer.go
@@ -1076,6 +1076,8 @@ func appendCommands(opts *Options, commandMap map[string][]commandsForTarget, ar
 		for _, buildFileName := range BuildFileNames {
 			if strings.HasSuffix(target, filepath.FromSlash("/"+buildFileName)) {
 				target = strings.TrimSuffix(target, filepath.FromSlash("/"+buildFileName)) + ":__pkg__"
+			} else if strings.HasSuffix(target, "/"+buildFileName) {
+				target = strings.TrimSuffix(target, "/"+buildFileName) + ":__pkg__"
 			}
 		}
 		var buildFiles []string

edit/buildozer_test.go
@@ -124,7 +124,7 @@ type targetExpressionToBuildFilesTestCase struct {
 	buildFiles      []string
 }
 
-func runTestTargetExpressionToBuildFiles(t *testing.T, buildFileName string) {
+func setupTestTmpWorkspace(t *testing.T, buildFileName string) (tmp string) {
 	tmp, err := ioutil.TempDir("", "")
 	if err != nil {
 		t.Fatal(err)
@@ -136,7 +136,6 @@ func runTestTargetExpressionToBuildFiles(t *testing.T, buildFileName string) {
 		t.Fatal(err)
 	}
 
-	defer os.RemoveAll(tmp)
 	if err := os.MkdirAll(filepath.Join(tmp, "a", "b"), 0755); err != nil {
 		t.Fatal(err)
 	}
@@ -158,6 +157,12 @@ func runTestTargetExpressionToBuildFiles(t *testing.T, buildFileName string) {
 	if err := ioutil.WriteFile(filepath.Join(tmp, "a", "c", buildFileName), nil, 0755); err != nil {
 		t.Fatal(err)
 	}
+	return
+}
+
+func runTestTargetExpressionToBuildFiles(t *testing.T, buildFileName string) {
+	tmp := setupTestTmpWorkspace(t, buildFileName)
+	defer os.RemoveAll(tmp)
 
 	for _, tc := range []targetExpressionToBuildFilesTestCase{
 		{tmp, "//", []string{filepath.Join(tmp, buildFileName)}},
@@ -203,6 +208,48 @@ func runTestTargetExpressionToBuildFiles(t *testing.T, buildFileName string) {
 func TestTargetExpressionToBuildFiles(t *testing.T) {
 	for _, buildFileName := range BuildFileNames {
 		runTestTargetExpressionToBuildFiles(t, buildFileName)
+	}
+}
+
+func runTestAppendCommands(t *testing.T, buildFileName string) {
+	tmp := setupTestTmpWorkspace(t, buildFileName)
+	defer os.RemoveAll(tmp)
+
+	for _, tc := range []targetExpressionToBuildFilesTestCase{
+		{tmp, ".:__pkg__", []string{"./" + buildFileName}},
+		{tmp, "a" + ":__pkg__", []string{"a/" + buildFileName}},
+		{"", "a" + ":__pkg__", []string{"a/" + buildFileName}},
+	} {
+		if tc.rootDir == "" {
+			cwd, err := os.Getwd()
+			if err != nil {
+				t.Fatal(err)
+			}
+			// buildozer should be able to find the WORKSPACE file in the current wd
+			if err := os.Chdir(tmp); err != nil {
+				t.Fatal(err)
+			}
+			defer os.Chdir(cwd)
+		}
+
+		commandsByFile := make(map[string][]commandsForTarget)
+		opts := NewOpts()
+		opts.RootDir = tc.rootDir
+		appendCommands(opts, commandsByFile, tc.buildFiles)
+		if len(commandsByFile) != 1 {
+			t.Errorf("Expect one target after appendCommands")
+		}
+		for _, value := range commandsByFile {
+			if value[0].target != tc.target {
+				t.Errorf("appendCommands for buildfile %s yielded target %s, expected %s", tc.buildFiles, value[0].target, tc.target)
+			}
+		}
+	}
+}
+
+func TestAppendCommands(t *testing.T) {
+	for _, buildFileName := range BuildFileNames {
+		runTestAppendCommands(t, buildFileName)
 	}
 }
 