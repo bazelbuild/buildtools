0d7ee3fba21ae7287bf68ad9a77d7caef48d0870
Fabian Meumertzheim <fabian@meumertzhe.im>
2023-01-03 16:14:40 +0100

    Group module extension proxy usages (#1112)
    In a `MODULE.bazel` file, sequences of statements defining or using the
    same module extension proxy object are no longer separated by blank
    lines. This includes:
    
    * Assignment expressions with a `use_extension` call as RHS
    * Call expressions with a dot expression as the callee
    * `use_repo` calls
    
    Example:
    ```
    go_deps = use_extension("//:extensions.bzl", "go_deps")
    go_deps.from_file(go_mod = "//:go.mod")
    use_repo(
        go_deps,
        "com_github_bazelbuild_buildtools",
        "com_github_google_go_cmp",
    )
    ```

--- buildifier/integration_test.sh
+++ buildifier/integration_test.sh
@@ -48,7 +48,44 @@ echo -e "not valid +" > test_dir/foo.bar
 mkdir test_dir/workspace  # name of a starlark file, but a directory
 mkdir test_dir/.git  # contents should be ignored
 echo -e "a+b" > test_dir/.git/git.bzl
-echo -e "module(name='my-module',version='1.0')\nbazel_dep(name='rules_cc',version='0.0.1')\nbazel_dep(name='protobuf',version='3.19.0')" > test_dir/MODULE.bazel
+cat > test_dir/MODULE.bazel <<'EOF'
+module(name='my-module',version='1.0')
+bazel_dep(name='rules_cc',version='0.0.1')
+bazel_dep(name='protobuf',version='3.19.0')
+bazel_dep(
+    name='rules_go',
+    version='0.37.0',
+    repo_name='io_bazel_rules_go',
+)
+go_sdk=use_extension("@io_bazel_rules_go//go:extensions.bzl","go_sdk")
+# Known to exist since it is instantiated by rules_go itself.
+use_repo(go_sdk,"go_default_sdk")
+non_module_deps = use_extension("//internal/bzlmod:non_module_deps.bzl","non_module_deps")
+use_repo(
+    non_module_deps,
+    "bazel_gazelle_go_repository_tools",
+    "bazel_gazelle_go_repository_config",
+    "bazel_gazelle_go_repository_cache",
+)
+rules_go_non_module_deps = use_extension("@io_bazel_rules_go//go/private:extensions.bzl","non_module_dependencies")
+use_repo(rules_go_non_module_deps,"go_googleapis")
+go_deps  =  use_extension("//:extensions.bzl",  "go_deps")
+go_deps.from_file(go_mod = "//:go.mod")
+use_repo(
+    go_deps,
+    "com_github_bazelbuild_buildtools",
+    "com_github_bmatcuk_doublestar_v4",
+    "com_github_fsnotify_fsnotify",
+    "com_github_google_go_cmp",
+    "com_github_pelletier_go_toml",
+    "com_github_pmezard_go_difflib",
+    "org_golang_x_mod",
+    "org_golang_x_sync",
+    "org_golang_x_tools",
+    # Used internally by the go_deps module extension.
+    "bazel_gazelle_go_repository_directives",
+)
+EOF
 
 cp test_dir/foo.bar golden/foo.bar
 cp test_dir/subdir/build golden/build
@@ -88,6 +125,44 @@ module(
 
 bazel_dep(name = "rules_cc", version = "0.0.1")
 bazel_dep(name = "protobuf", version = "3.19.0")
+bazel_dep(
+    name = "rules_go",
+    repo_name = "io_bazel_rules_go",
+    version = "0.37.0",
+)
+
+go_sdk = use_extension("@io_bazel_rules_go//go:extensions.bzl", "go_sdk")
+
+# Known to exist since it is instantiated by rules_go itself.
+use_repo(go_sdk, "go_default_sdk")
+
+non_module_deps = use_extension("//internal/bzlmod:non_module_deps.bzl", "non_module_deps")
+use_repo(
+    non_module_deps,
+    "bazel_gazelle_go_repository_tools",
+    "bazel_gazelle_go_repository_config",
+    "bazel_gazelle_go_repository_cache",
+)
+
+rules_go_non_module_deps = use_extension("@io_bazel_rules_go//go/private:extensions.bzl", "non_module_dependencies")
+use_repo(rules_go_non_module_deps, "go_googleapis")
+
+go_deps = use_extension("//:extensions.bzl", "go_deps")
+go_deps.from_file(go_mod = "//:go.mod")
+use_repo(
+    go_deps,
+    "com_github_bazelbuild_buildtools",
+    "com_github_bmatcuk_doublestar_v4",
+    "com_github_fsnotify_fsnotify",
+    "com_github_google_go_cmp",
+    "com_github_pelletier_go_toml",
+    "com_github_pmezard_go_difflib",
+    "org_golang_x_mod",
+    "org_golang_x_sync",
+    "org_golang_x_tools",
+    # Used internally by the go_deps module extension.
+    "bazel_gazelle_go_repository_directives",
+)
 EOF
 
 diff test_dir/BUILD golden/BUILD.golden
