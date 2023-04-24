aa477c7769af31f7b29a1a722c135d86fab83ab6
Fabian Meumertzheim <fabian@meumertzhe.im>
2023-04-24 16:39:52 +0200

    Fix empty `package()` calls appearing in non-BUILD files (#1150)
    * Fix empty `package()` calls appearing in non-BUILD files
    
    Caused by https://github.com/bazelbuild/buildtools/commit/9c3c1fc0106ed2174f1a7c4a322a08f0a553bc1e,
    but only relevant if users use the target `__pkg__` to skip target-level
    processing for non-BUILD files.

--- buildozer/buildozer_test.sh
+++ buildozer/buildozer_test.sh
@@ -2059,6 +2059,20 @@ EOF
   diff -u MODULE.bazel.expected MODULE.bazel || fail "Output didn't match"
 }
 
+function test_module_bazel_new() {
+  cat > MODULE.bazel <<EOF
+bazel_dep(name = "gazelle", version = "0.30.0")
+EOF
+
+  cat > MODULE.bazel.expected <<EOF
+bazel_dep(name = "gazelle", version = "0.30.0")
+bazel_dep(name = "rules_go")
+EOF
+
+  $buildozer 'new bazel_dep rules_go after gazelle' //MODULE.bazel:__pkg__
+  diff -u MODULE.bazel.expected MODULE.bazel || fail "Output didn't match"
+}
+
 function test_use_repo_add() {
   cat > MODULE.bazel <<EOF
 module(
