c229f559c800acd0064807ee474372ddb1789390
G Sarath Chandra <gsc.603@gmail.com>
2023-09-07 02:02:20 +0530

    Updated the bazel query syntax
    Updated the quotations syntax following the example at https://bazel.build/query/guide#tests-beneath-foo

--- unused_deps/unused_deps.go
+++ unused_deps/unused_deps.go
@@ -355,7 +355,7 @@ func main() {
 	}
 	queryCmd = append(queryCmd, blazeFlags...)
 	queryCmd = append(
-		queryCmd, fmt.Sprintf("kind('(kt|java|android)_*', %s)", strings.Join(targetPatterns, " + ")))
+		queryCmd, fmt.Sprintf("'kind(\"(kt|java|android)_*\", %s)'", strings.Join(targetPatterns, " + ")))
 
 	log.Printf("running: %s %s", *buildTool, strings.Join(queryCmd, " "))
 	queryOut, err := cmdWithStderr(*buildTool, queryCmd...).Output()
